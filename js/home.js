a833914423b2864c609b9c987253845f3a79183d
Jack Loughran <loughranjack28@gmail.com>
2016-07-10 12:54:04 -0400

    links work

--- js/home.js
+++ js/home.js
@@ -2,10 +2,31 @@
   'use strict';
   
   angular
-    .module('homeApp', [])
+    .module('homeApp', ['ngRoute'])
+    .config(routeConfig)
     .controller('HomeController', SinglePageApp);
   
   SinglePageApp.$inject = ['$scope'];
+  
+  function routeConfig($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '_pages/home.html',
+        controller: 'HomeController'
+      })
+      .when('/about', {
+      templateUrl: '_pages/about.html',
+      controller: 'HomeController'
+      })
+      .when('/projects', {
+      templateUrl: '_pages/projects.html',
+      controller: 'HomeController'
+      })
+      .when('/contact', {
+      templateUrl: '_pages/contact.html',
+      controller: 'HomeController'
+      });
+  }
   
   function SinglePageApp($scope) {
     var vm = this;
