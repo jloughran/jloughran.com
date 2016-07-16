cb226192fa8a082242632597a1bbbb926ec0525e
Jack Loughran <loughranjack28@gmail.com>
2016-07-16 12:22:36 -0400

    active links if you refresh the page

--- js/home.js
+++ js/home.js
@@ -1,49 +1,55 @@
-(function () {
-  'use strict';
-  
-  angular
-    .module('homeApp', ['ngRoute'])
-    .config(routeConfig)
-    .controller('HomeController', SinglePageApp);
-  
-  SinglePageApp.$inject = ['$scope', '$location'];
-  
-  function routeConfig($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: '_pages/home.html',
-        controller: 'HomeController'
-      })
-      .when('/about', {
-      templateUrl: '_pages/about.html',
+angular
+  .module('homeApp', ['ngRoute'])
+  .config(routeConfig)
+  .controller('HomeController', SinglePageApp);
+
+SinglePageApp.$inject = ['$scope', '$location'];
+
+function routeConfig($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '_pages/home.html',
       controller: 'HomeController'
-      })
-      .when('/projects', {
-      templateUrl: '_pages/projects.html',
-      controller: 'HomeController'
-      })
-      .when('/contact', {
-      templateUrl: '_pages/contact.html',
-      controller: 'HomeController'
-      });
-  }
-  
-  function SinglePageApp($scope, $location) {
-    var vm = this;
-    
-    vm.menuLinks = [
-      {
-        url: '#/about',
-        text: 'About'
-      },
-      {
-        url: '#/projects',
-        text: 'Projects'
-      },
-      {
-        url: '#/contact',
-        text: 'Contact'
-      }
-    ];
-  }
-})();
+    })
+    .when('/about', {
+    templateUrl: '_pages/about.html',
+    controller: 'HomeController'
+    })
+    .when('/projects', {
+    templateUrl: '_pages/projects.html',
+    controller: 'HomeController'
+    })
+    .when('/contact', {
+    templateUrl: '_pages/contact.html',
+    controller: 'HomeController'
+    });
+}
+
+function SinglePageApp($scope, $location) {
+  var vm = this;
+
+  vm.menuLinks = [
+    {
+      url: '#/about',
+      text: 'About',
+    },
+    {
+      url: '#/projects',
+      text: 'Projects',
+    },
+    {
+      url: '#/contact',
+      text: 'Contact',
+    }
+  ];
+
+  const path = '#' + $location.path();
+  vm.menuLinks.forEach(function (link) {
+    if (link.url === path) {
+      link.style = 'menu-active';
+    }
+    else {
+      link.style = 'menu-inactive';
+    }
+  });
+}
